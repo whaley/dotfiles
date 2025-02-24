8d05a46ba9b3c5f7600a3526e249d602e95af06d
Jason Whaley <whaley@morninghacks.com>
2025-02-24 07:36:28 -0500

    aerospace: change scripts to focus after move for obsidian/workplace

--- bin/aerospace_move_workplace_to_current_workspace.py
+++ bin/aerospace_move_workplace_to_current_workspace.py
@@ -12,38 +12,54 @@ from typing import List, Optional
 
 AEROSPACE = "/Users/whaley/homebrew/bin/aerospace"
 
+
 def run_command(cmd: List[str]) -> str:
-  res = subprocess.run(cmd, capture_output=True)
-  return res.stdout.decode("utf-8") if res.stdout else ""
+    res = subprocess.run(cmd, capture_output=True)
+    return res.stdout.decode("utf-8") if res.stdout else ""
+
 
 def find_workplace_window_id() -> Optional[int]:
-  stdout = run_command([AEROSPACE, "list-windows", "--all", "--json"])
-  if not stdout:
-    return None
+    stdout = run_command([AEROSPACE, "list-windows", "--all", "--json"])
+    if not stdout:
+        return None
 
+    # Output is in the following form.
+    # [
+    #   {
+    #     "app-name" : "Workplace",
+    #     "window-title" : "",
+    #     "window-id" : 1234
+    #   }
+    #   ...
+    # ]
 
-  # Output is in the following form.
-  # [
-  #   {
-  #     "app-name" : "Workplace",
-  #     "window-title" : "",
-  #     "window-id" : 1234
-  #   }
-  #   ...
-  # ]
+    all_windows_json = json.loads(stdout)
+    filtered = [app for app in all_windows_json if app["app-name"] == "Workplace"]
+    return filtered[0].get("window-id", "") if filtered else None
 
-  all_windows_json = json.loads(stdout)
-  filtered = [app for app in all_windows_json if app["app-name"] == "Workplace"]
-  return filtered[0].get("window-id","") if filtered else None
 
 def find_active_workspace_name() -> str:
-  stdout_lines = run_command([AEROSPACE, "list-workspaces", "--focused"])
-  return stdout_lines[0]
+    stdout_lines = run_command([AEROSPACE, "list-workspaces", "--focused"])
+    return stdout_lines[0]
+
 
 if __name__ == "__main__":
-  workplace_window_id = find_workplace_window_id()
-  if workplace_window_id:
-    active_workspace_name = find_active_workspace_name()
-    subprocess.run([AEROSPACE, "move-node-to-workspace", "--window-id", str(workplace_window_id), active_workspace_name, "--fail-if-noop",])
-  else:
-    subprocess.run(["open","/Users/whaley/Applications/Chrome Apps.localized/Workplace.app"])
+    workplace_window_id = find_workplace_window_id()
+    if workplace_window_id:
+        active_workspace_name = find_active_workspace_name()
+        subprocess.run(
+            [
+                AEROSPACE,
+                "move-node-to-workspace",
+                "--window-id",
+                str(workplace_window_id),
+                active_workspace_name,
+                "--fail-if-noop",
+            ]
+        )
+        subprocess.run([AEROSPACE, "focus", "--window-id", str(workplace_window_id)])
+
+    else:
+        subprocess.run(
+            ["open", "/Users/whaley/Applications/Chrome Apps.localized/Workplace.app"]
+        )
